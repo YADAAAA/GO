43bcf98e7f3525cd9286d5216339c3688f19473e
YADAAAA <6306021610039@fitm.kmutnb.ac.th>
2022-05-18 23:19:34 +0700

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yadaaaa/hello
+
+go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
-package main
-
-import "fmt"
+//เริ่มต้นการเขียนโรแกรมภาษา go
+package main //ประกาศ
+import "fmt" //เรียกใช้งาน library
 
 func main() {
 	fmt.Print("Hello World")
 }
+
+//การใช้ Go command
+//ใช้คำสั่ง go mod init github.com/yadaaaa/ชื่ออะไรก็ได้ ลงใน terminal เพื่อเก็บพาท
+//พิมพ์ pk แล้วกด tab เพื่อขึ้นโครงสร้างของโค้ด
