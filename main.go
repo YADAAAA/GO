8bc77be22a715b31aa0b66131ccc2c6452d39739
YADAAAA <6306021610039@fitm.kmutnb.ac.th>
2022-05-18 23:25:19 +0700

    update

--- main.go
+++ main.go
@@ -9,3 +9,5 @@ func main() {
 //การใช้ Go command
 //ใช้คำสั่ง go mod init github.com/yadaaaa/ชื่ออะไรก็ได้ ลงใน terminal เพื่อเก็บพาท
 //พิมพ์ pk แล้วกด tab เพื่อขึ้นโครงสร้างของโค้ด
+//พิมพ์ fp แล้วกด tab เพื่อขึ้น fmt.Print("Hello World")
+//พิมพ์ go run main.go เพื่อรันโปรแกรมไฟล์นี้
