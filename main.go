9a2c8414fbf1f94370adf31e09f4f4d6e49b73ae
YADAAAA <6306021610039@fitm.kmutnb.ac.th>
2022-05-19 11:44:50 +0700

    update

--- main.go
+++ main.go
@@ -13,3 +13,4 @@ func main() {
 //พิมพ์ go run main.go เพื่อรันโปรแกรมไฟล์นี้
 //ctrl+ship+p แล้วหาคำว่า Insert Snippet เพื่อพิมพ์เร็วขึ้น!
 //ใช้คำสั่ง go build .\main.go เพื่อใช้เป็น library .exe รันในเครื่องอื่นได้
+//ใช้สำคั่ง go get github.com/liudng/dogo เพื่อเรียก package จากด้านนอก -> dogo รันอัตโนมัติทุกครั้งที่มีการเปลี่ยนแปลงโค้ด และสร้างไฟล์ go.sum ขึ้นมา อย่าลืมสร้าง dogo.json
